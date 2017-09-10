58d25820b7a7b88a8949186e0be427347f1cca8d
mattia richetto <mattia.richetto@gmail.com>
2017-09-09 22:46:08 -0700

    add cli and proxy

--- src/app.js
+++ src/app.js
@@ -1,13 +1,21 @@
 
 const express = require('express');
+const proxy = require('http-proxy-middleware');
 const path = require('path');
 const index = require('./routes/index');
 
 const app = express();
 
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'pug');
+if (process.env.API_BASE_URL && process.env.PROXY_TARGET) {
+  app.use(
+    process.env.API_BASE_URL,
+    proxy({ target: process.env.PROXY_TARGET, changeOrigin: true })
+  );
+}
 
 app.use('/', index);
+
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'pug');
 
 module.exports = app;
