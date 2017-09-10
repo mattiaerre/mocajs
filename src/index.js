34a60e114638164926198621d9b935e4fb3d2f4e
mattia richetto <mattia.richetto@gmail.com>
2017-09-09 21:33:37 -0700

    almost there

---
+++ src/index.js
@@ -0,0 +1,10 @@
+
+const debug = require('debug')('mocajs:index');
+require('dotenv').config();
+const app = require('./app');
+
+const port = process.env.PORT || 9000;
+
+app.listen(port);
+
+debug(`http://localhost:${port}`);
---
+++ src/routes/index.js
@@ -0,0 +1,39 @@
+const debug = require('debug')('mocajs:routes/index');
+const express = require('express');
+const Client = require('oc-client');
+
+const client = new Client(
+  {
+    registries: { serverRendering: process.env.OC_REGISTRY_BASE_URL }
+  }
+);
+
+const components = [
+  {
+    name: process.env.OC_COMPONENT_NAME || (() => { throw new Error('the OpenComponents component\'s name is missing'); })(),
+    version: process.env.OC_COMPONENT_VERSION || 'x.x.x',
+    parameters: process.env.OC_COMPONENT_PARAMETERS || {}
+  }
+];
+
+const router = express.Router();
+
+router.get('/', (req, res) => {
+  client.renderComponents(
+    components,
+    {
+      container: false,
+      headers: {
+        'Accept-Language': 'en-US'
+      },
+      timeout: 3.0
+    }, (errors, htmls) => {
+      if (errors) {
+        debug('errors:', errors);
+      }
+      debug('htmls[0]:', htmls[0]);
+      res.render('index', { title: process.env.OC_COMPONENT_NAME, component: htmls[0] });
+    });
+});
+
+module.exports = router;
